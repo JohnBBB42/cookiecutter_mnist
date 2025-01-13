2711b16e43f0dd8889cf58fd18586355f4b28975
Jonatan Emil Svendsen <Jhz209@alumni.ku.dk>
2025-01-13 13:14:56 +0100

    bugfix

--- scripts/link_model.py
+++ scripts/link_model.py
@@ -2,13 +2,14 @@ import os
 import typer
 import wandb
 
+
 def link_model(
     artifact_path: str = typer.Argument(..., help="Artifact path in the form entity/project/artifact:version"),
     aliases: list[str] = typer.Option(
         ["staging"],  # Default value
-        "--alias",    # Long option name
-        "-a",         # Short option name
-        help="Aliases to link the artifact with. Can be repeated."
+        "--alias",  # Long option name
+        "-a",  # Short option name
+        help="Aliases to link the artifact with. Can be repeated.",
     ),
 ) -> None:
     """
@@ -29,12 +30,10 @@ def link_model(
     artifact_name, _ = artifact_name_version.split(":")
 
     artifact = api.artifact(artifact_path)
-    artifact.link(
-        target_path=f"{os.getenv('WANDB_ENTITY')}/model-registry/{artifact_name}",
-        aliases=aliases
-    )
+    artifact.link(target_path=f"{os.getenv('WANDB_ENTITY')}/model-registry/{artifact_name}", aliases=aliases)
     artifact.save()
     typer.echo(f"Artifact {artifact_path} linked to {aliases}")
+
 
 if __name__ == "__main__":
     typer.run(link_model)
