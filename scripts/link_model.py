0e77462637d388699252b43900ff225704fafc9c
Jonatan Emil Svendsen <Jhz209@alumni.ku.dk>
2025-01-13 12:47:58 +0100

    bugfix

--- scripts/link_model.py
+++ scripts/link_model.py
@@ -1,24 +1,23 @@
 import os
-
 import typer
-
 import wandb
 
-
-def link_model(artifact_path: str, aliases: list[str] = ["staging"]) -> None:
+def link_model(
+    artifact_path: str = typer.Argument(..., help="Artifact path in the form entity/project/artifact:version"),
+    aliases: list[str] = typer.Option(
+        ["staging"],  # Default value
+        "--alias",    # Long option name
+        "-a",         # Short option name
+        help="Aliases to link the artifact with. Can be repeated."
+    ),
+) -> None:
     """
     Stage a specific model to the model registry.
 
-    Args:
-        artifact_path: Path to the artifact to stage.
-            Should be of the format "entity/project/artifact_name:version".
-        aliases: List of aliases to link the artifact with.
-
     Example:
-        model_management link-model entity/project/artifact_name:version -a staging -a best
-
+        python link_model.py entity/project/artifact:version -a staging -a production
     """
-    if artifact_path == "":
+    if not artifact_path:
         typer.echo("No artifact path provided. Exiting.")
         return
 
@@ -30,10 +29,12 @@ def link_model(artifact_path: str, aliases: list[str] = ["staging"]) -> None:
     artifact_name, _ = artifact_name_version.split(":")
 
     artifact = api.artifact(artifact_path)
-    artifact.link(target_path=f"{os.getenv('WANDB_ENTITY')}/model-registry/{artifact_name}", aliases=aliases)
+    artifact.link(
+        target_path=f"{os.getenv('WANDB_ENTITY')}/model-registry/{artifact_name}",
+        aliases=aliases
+    )
     artifact.save()
     typer.echo(f"Artifact {artifact_path} linked to {aliases}")
-
 
 if __name__ == "__main__":
     typer.run(link_model)
